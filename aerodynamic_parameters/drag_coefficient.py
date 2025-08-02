6affc768cc89998ae50d09a0381ffd5faf563449
rafaelino1707 <up202406634@up.pt>
2025-08-02 19:22:40 +0100

    Obtaining Non-Dimensional Coefficients

---
+++ aerodynamic_parameters/drag_coefficient.py
@@ -0,0 +1,14 @@
+import math
+from aerodynamic_parameters.dynamic_pressure import *
+
+def drag_per_span(A, N, Angle_of_Atack):
+    alpha = math.radians(Angle_of_Atack)
+    L = N*math.cos(alpha) - A*math.sin(alpha)
+
+
+def drag_coefficient(A, N, Angle_of_Atack, V_inf, rho_inf, chord_length, wing_span):
+    L_per_span = drag_per_span(A, N, Angle_of_Atack)
+
+    dynamic_press = dynamic_pressure(rho_inf, V_inf)
+
+    return L_per_span / (dynamic_press * chord_length)
